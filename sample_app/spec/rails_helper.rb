c6db36a2015b1ceb5530438648240ab582812c6a
Tatsuya Kobayashi <pikopiko28@gmail.com>
2017-02-23 08:36:40 +0000

    Add direct conversion of controller spec

--- sample_app/spec/rails_helper.rb
+++ sample_app/spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -56,16 +58,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
 
-include ApplicationHelper
-
-# テストユーザーがログイン中の場合にtrueを返す
-def is_logged_in?
-  !session[:user_id].nil?
-end
-
-# テストユーザーとしてログインする
-def log_in_as(user)
-  session[:user_id] = user.id
+  config.include ApplicationHelper
+  config.include LoginMacros
 end
